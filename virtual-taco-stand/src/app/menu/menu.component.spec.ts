c0fbcbefe9131b2ce1b1e426f253746f2bc99bb8
Shannon Kueneke <slkueneke@gmail.com>
2026-04-11 11:51:05 -0700

    virtual taco stand

--- virtual-taco-stand/src/app/menu/menu.component.spec.ts
+++ virtual-taco-stand/src/app/menu/menu.component.spec.ts
@@ -17,7 +17,20 @@ describe('MenuComponent', () => {
     fixture.detectChanges();
   });
 
+  /**
+   * Created by default when the component is generated
+   * Unit Test 1
+   */
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  /**
+   * Unit Test 2
+   */
+  it('should correctly display a list of tacos', () => {
+    const compiled = fixture.nativeElement as HTMLElement; //get the compiled html of the component
+    const menuItems = compiled.querySelectorAll('.menu-item'); //get all the menu items
+    expect(menuItems.length).toEqual(component.menu.length); //check if the number of menu items is equal to the number of items in the menu array
   });
 });
