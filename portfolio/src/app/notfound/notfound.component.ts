690087f84fd93956e3ab6e21b5c16ac0c4b79349
Shannon Kueneke <slkueneke@gmail.com>
2026-04-29 10:39:15 -0700

    portfolio components

---
+++ portfolio/src/app/notfound/notfound.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      notfound works!
+    </p>
+  `,
+  styles: ``
+})
+export class NotfoundComponent {
+
+}
