8d5da5530b6d902a660fc45c23d2e50a35bba012
Shannon Kueneke <slkueneke@gmail.com>
2026-04-30 08:34:35 -0700

    week 6

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Bungee+Shade&family=Google+Sans+Code:ital,wght,MONO@0,300..800,1;1,300..800,1&family=Quicksand:wght@300..700&family=Tourney:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+  `,
+  styles: [``],
 })
 export class AppComponent {
   title = 'portfolio';
