d3a107c159f5f7739fdbef5429db8caaaa3cb773
Shannon Kueneke <slkueneke@gmail.com>
2026-05-11 14:15:03 -0700

    home page content dotcom/mobile

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -11,11 +11,9 @@ import { Router, RouterLink } from '@angular/router';
         <section>
           <h2 class="font-bubble">Bucket List</h2>
           <ul class="font-clean">
-            <li>item 1</li>
-            <li>item 2</li>
-            <li>item 3</li>
-            <li>item 4</li>
-            <li>item 5</li>
+            <li>Create a native app</li>
+            <li>Master Python</li>
+            <li>Learn React</li>
           </ul>
         </section>
       </section>
@@ -36,26 +34,68 @@ import { Router, RouterLink } from '@angular/router';
       </section>
     </div>
   `,
-  styles: [`
-    .colLeft ul {
-      list-style:none;
-    }
+  styles: [
+    `
+      .colLeft ul {
+        list-style: none;
+      }
 
-    .colLeft li::before {
-      content: '> ';
-    }
+      .colLeft li::before {
+        content: '> ';
+      }
 
-    .colLeft > section {
-      padding:2em;
-    }
+      .colLeft > section {
+        padding: 2em;
+      }
 
-    .colRight > section {
-      padding:3em;
-    }
+      .colRight > section {
+        padding: 3em;
+      }
 
-    .colRight h2 {
-      font-size:2em;
-    }
-    `],
+      .colRight h2 {
+        font-size: 2em;
+      }
+
+      @media all and (max-width: 576px) {
+        .flexWrapper {
+          flex-direction: column;
+        }
+
+        .colRight {
+          order: 1;
+          width: 100%;
+        }
+
+        .colRight p {
+          font-size: 1.5em;
+        }
+
+        .colLeft {
+          order: 2;
+          width: 100%;
+          margin-bottom: 4em;
+        }
+
+        .colLeft h2 {
+          text-align: center;
+          font-size: 2em;
+        }
+
+        .colLeft ul {
+          font-size: 1.25em;
+          width: fit-content;
+          margin: 0 auto;
+        }
+
+        .cta {
+          width: 100%;
+          display: block;
+          max-width: unset;
+          margin-bottom: 1.5em;
+          font-size: 1.25em;
+        }
+      }
+    `,
+  ],
 })
 export class HomeComponent {}
